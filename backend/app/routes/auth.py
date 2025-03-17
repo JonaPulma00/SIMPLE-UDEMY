26f69c5b48df5ee15e5d318afe933587221cfef5
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-17 19:04:43 +0100

    fixed gramatical boolean issue

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -11,7 +11,7 @@ router = APIRouter()
 @router.post("/register")
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
     db_user = await register_user(db, user)
-    return {"succes": true, "message": "Usuario registrado exitosamente"}
+    return {"succes": True, "message": "Usuario registrado exitosamente"}
 
 # Login 
 @router.post("/login", response_model=Token)
