51e251b851a6dd281bb1ff4a57540419556adab8
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-17 18:36:25 +0100

    fixed errors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 class Settings:
@@ -11,7 +12,12 @@ class Settings:
   DB_PORT: int = int(os.getenv("DB_PORT"))
   SECRET_KEY: str = os.getenv("SECRET_KEY")
   JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")
-  ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1200))
-  REFRESH_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 604800))
+  ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1200"))
+  REFRESH_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", "604800"))
 
 settings = Settings()
+
+SECRET_KEY = settings.SECRET_KEY
+JWT_ALGORITHM = settings.JWT_ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+REFRESH_TOKEN_EXPIRE_MINUTES = settings.REFRESH_TOKEN_EXPIRE_MINUTES
