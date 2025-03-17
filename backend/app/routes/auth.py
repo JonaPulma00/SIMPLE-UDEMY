e4755b940e1562b5623af7fd022174e26e06d208
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-17 19:02:13 +0100

    changed jwt logic

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -7,35 +7,33 @@ from app.utils.security import create_access_token, create_refresh_token
 
 router = APIRouter()
 
-# Register
-@router.post("/register", response_model=Token)
+# Register 
+@router.post("/register")
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
-  db_user = await register_user(db,user)
-  access_token = create_access_token({"sub": db_user.user_id})
-  refresh_token = create_refresh_token({"sub": db_user.user_id})
-  return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+    db_user = await register_user(db, user)
+    return {"succes": true, "message": "Usuario registrado exitosamente"}
 
 # Login 
 @router.post("/login", response_model=Token)
 async def login(user: UserLogin, db: AsyncSession = Depends(get_db)):
-  db_user = await authenticate_user(db, user.username, user.password)
-  access_token = create_access_token({"sub": db_user.user_id})
-  refresh_token = create_refresh_token({"sub": db_user.user_id})
-  return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+    db_user = await authenticate_user(db, user.username, user.password)
+    access_token = create_access_token({"sub": db_user.user_id})
+    refresh_token = create_refresh_token({"sub": db_user.user_id})
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
 # Refresh token
 @router.post("/refresh", response_model=Token)
 async def refresh(refresh_token: str):
-  try:
-    payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[JWT_ALGORITHM])
-    user_id = payload.get("sub")
-    if user_id is None:
+    try:
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-    new_tokens = {
+        new_tokens = {
             "access_token": create_access_token({"sub": user_id}),
             "refresh_token": create_refresh_token({"sub": user_id}),
             "token_type": "bearer"
-    }
-    return new_tokens
-  except jwt.PyJWTError:
-     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        }
+        return new_tokens
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
