11e0bf7340df561bd1228af7662371cf4f08d1dc
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-20 16:49:25 +0100

    adding login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,13 +1,31 @@
+import { NavLink } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 export const Login = () => {
-
-
   const navigate = useNavigate()
-
-
   return (
     <>
-      <div>Login</div>
+      <div class="wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div class="input-box">
+            <input type="text" placeholder="username" />
+            <i class='bx bxs-user'></i>
+          </div>
+          <div class="input-box">
+            <input type="password" placeholder="Password" />
+            <i class='bx bxs-lock-alt'></i>
+          </div>
+          <div class="remember-forget">
+            <label ><input type="checkbox" />
+              Remember me</label>
+            <NavLink to='/' > Forgot Password?</NavLink>
+          </div>
+          <button type="submit" class="btn">Log in</button>
+          <div class="register-link">
+            <p>Don't have a account?  <NavLink to='/register' >Register</NavLink></p>
+          </div>
+        </form>
+      </div>
       <button onClick={() => navigate(-1)}>Go Back</button>
       <button onClick={() => navigate(1)}>Go Forward</button>
     </>
