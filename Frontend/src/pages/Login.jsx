0a2af9f1bc3a689d6e1bdf34eedcd6f08681dbd6
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-20 16:59:37 +0100

    minor change

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export const Login = () => {
           <form action="">
             <h1>Login</h1>
             <div className="input-box">
-              <input type="text" placeholder="username" />
+              <input type="text" placeholder="Username" />
               <i className='bx bxs-user'></i>
             </div>
             <div className="input-box">
