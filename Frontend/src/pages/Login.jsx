a35b3afb150b45718a9b6a8c902fcd8b4dbcd4df
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-20 16:58:35 +0100

    adding styles

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -5,27 +5,29 @@ export const Login = () => {
   const navigate = useNavigate()
   return (
     <>
-      <div className="wrapper">
-        <form action="">
-          <h1>Login</h1>
-          <div className="input-box">
-            <input type="text" placeholder="username" />
-            <i className='bx bxs-user'></i>
-          </div>
-          <div className="input-box">
-            <input type="password" placeholder="Password" />
-            <i className='bx bxs-lock-alt'></i>
-          </div>
-          <div className="remember-forget">
-            <label ><input type="checkbox" />
-              Remember me</label>
-            <NavLink to='/' > Forgot Password?</NavLink>
-          </div>
-          <button type="submit" className="btn">Log in</button>
-          <div className="register-link">
-            <p>Don't have a account?  <NavLink to='/register' >Register</NavLink></p>
-          </div>
-        </form>
+      <div className="general-container">
+        <div className="wrapper">
+          <form action="">
+            <h1>Login</h1>
+            <div className="input-box">
+              <input type="text" placeholder="username" />
+              <i className='bx bxs-user'></i>
+            </div>
+            <div className="input-box">
+              <input type="password" placeholder="Password" />
+              <i className='bx bxs-lock-alt'></i>
+            </div>
+            <div className="remember-forget">
+              <label ><input type="checkbox" />
+                Remember me</label>
+              <NavLink to='/' > Forgot Password?</NavLink>
+            </div>
+            <button type="submit" className="btn">Log in</button>
+            <div className="register-link">
+              <p>Don't have a account?  <NavLink to='/register' >Register</NavLink></p>
+            </div>
+          </form>
+        </div>
       </div>
       <button onClick={() => navigate(-1)}>Go Back</button>
       <button onClick={() => navigate(1)}>Go Forward</button>
