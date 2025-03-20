f37dc28cc4212178baf41a410d9ed41cb3b39178
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-20 15:47:45 +0100

    adding config

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import "../styles/Footer.css"
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 export const Footer = () => {
   return (
     <>
---
+++ Frontend/src/config/config.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+  BASE_API_URL: process.env.BASE_API_URL,
+};
