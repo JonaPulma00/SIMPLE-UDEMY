e8e860288c24951688189dbf5f25b5376b2dbddf
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-20 15:51:08 +0100

    adding register user func

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,1 +1,12 @@
 import axios from "axios";
+import { config } from "../config/appConfig";
+
+export const registerUser = {
+  register: (user, email, password) => {
+    return axios.post(`${config.BASE_API_URL}/auth/register`, {
+      user,
+      email,
+      password,
+    });
+  },
+};
