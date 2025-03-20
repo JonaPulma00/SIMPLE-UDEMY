91aa4f1fb0fb55d5d0f2f90c21f56e687284c8d6
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-20 15:57:15 +0100

    validate user function

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -2,10 +2,19 @@ import axios from "axios";
 import { config } from "../config/appConfig";
 
 export const registerUser = {
-  register: (user, email, password) => {
+  register: (username, email, password) => {
     return axios.post(`${config.BASE_API_URL}/auth/register`, {
-      user,
+      username,
       email,
+      password,
+    });
+  },
+};
+
+export const validateUser = {
+  login: (username, password) => {
+    return axios.post(`${config.BASE_API_URL}/auth/login`, {
+      username,
       password,
     });
   },
