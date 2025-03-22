e5aa842c1ad1649fefdf890d1927d95f8404ee8c
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-22 23:06:05 +0100

    removed bad use of useMemo

--- Frontend/src/components/ParticlesComponent.jsx
+++ Frontend/src/components/ParticlesComponent.jsx
@@ -20,73 +20,70 @@ const ParticlesComponent = (props) => {
   };
 
 
-  const options = useMemo(
-    () => ({
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "repulse",
-          },
-          onHover: {
-            enable: true,
-            mode: 'grab',
-          },
+  const options = {
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "repulse",
         },
-        modes: {
-          push: {
-            distance: 200,
-            duration: 15,
-          },
-          grab: {
-            distance: 150,
-          },
+        onHover: {
+          enable: true,
+          mode: 'grab',
         },
       },
-      particles: {
-        color: {
-          value: "#FFFFFF",
+      modes: {
+        push: {
+          distance: 200,
+          duration: 15,
         },
-        links: {
-          color: "#FFFFFF",
+        grab: {
           distance: 150,
-          enable: true,
-          opacity: 0.3,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          random: true,
-          speed: 1,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-            value: 1200,
-          },
-          value: 100,
-        },
-        opacity: {
-          value: 1.0,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 3 },
         },
       },
-      detectRetina: true,
-    }),
-    [],
-  );
+    },
+    particles: {
+      color: {
+        value: "#FFFFFF",
+      },
+      links: {
+        color: "#FFFFFF",
+        distance: 150,
+        enable: true,
+        opacity: 0.3,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: true,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value: 1200,
+        },
+        value: 120,
+      },
+      opacity: {
+        value: 1.0,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 3 },
+      },
+    },
+    detectRetina: true,
 
+  }
 
   return <Particles id={props.id} init={particlesLoaded} options={options} />;
 };
