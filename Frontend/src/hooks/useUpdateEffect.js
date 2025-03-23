f2e0b5545968b965ef28e7b50a41178e98c52c87
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-23 22:38:41 +0100

    new custom hooks

---
+++ Frontend/src/hooks/useUpdateEffect.js
@@ -0,0 +1,12 @@
+import { useRef, useEffect } from "react";
+
+export default function useUpdateEffect(callback, dependencies) {
+  const firstRenderRef = useRef(true);
+  useEffect(() => {
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false;
+      return;
+    }
+    return callback();
+  }, dependencies);
+}
