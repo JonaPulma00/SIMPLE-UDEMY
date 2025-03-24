6f4683b053526d6c66793d3842e2c0e3aeabfaf1
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-24 19:48:38 +0100

    basic implementation useForm

--- Frontend/src/pages/InstructorRegister.jsx
+++ Frontend/src/pages/InstructorRegister.jsx
@@ -1,6 +1,16 @@
 import { Navbar } from "../components/Navbar"
 import { ParticlesComponent } from "../components/ParticlesComponent"
+import { useForm } from "../hooks/useForm"
+
 export const InstructorRegister = () => {
+
+  const initialForm = {
+    username: '',
+    email: '',
+    password: '',
+    is_instructor: ''
+  }
+  const { username, email, password, is_instructor, onInputChange, formState } = useForm()
   return (
     <>
       <div className="general-wrapper">
