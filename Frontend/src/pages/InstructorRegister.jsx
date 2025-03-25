b43074f2361196c2ef263ab037a9367367f7dfca
Jonathan <jrodas753@boscdelacoma.cat>
2025-03-25 18:14:23 +0100

    current v

--- Frontend/src/pages/InstructorRegister.jsx
+++ Frontend/src/pages/InstructorRegister.jsx
@@ -1,16 +1,20 @@
 import { Navbar } from "../components/Navbar"
 import { ParticlesComponent } from "../components/ParticlesComponent"
 import { useForm } from "../hooks/useForm"
+import { useNavigate } from "react-router-dom"
+import '../styles/user/authForms.css'
 
 export const InstructorRegister = () => {
-
+  const navigate = useNavigate()
   const initialForm = {
     username: '',
     email: '',
     password: '',
     is_instructor: ''
   }
-  const { username, email, password, is_instructor, onInputChange, formState } = useForm()
+
+  const { username, email, password, is_instructor, onInputChange, formState } = useForm(initialForm)
+
   return (
     <>
       <div className="general-wrapper">
