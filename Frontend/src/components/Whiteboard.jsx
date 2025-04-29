ab7080dde1ade083370dab854519f43152f4d956
Jonathan <jona.rierasanchez@gmail.com>
2025-04-29 17:34:10 +0200

    adding whiteboard

--- Frontend/src/components/Whiteboard.jsx
+++ Frontend/src/components/Whiteboard.jsx
@@ -1,10 +1,56 @@
-import { Stage } from "react-konva"
+import { Stage, Layer, Line } from "react-konva";
+import { useState, useRef } from "react";
+import "../styles/global/Whiteboard.css";
 
 export const Whiteboard = () => {
+  const [lines, setLines] = useState([]);
+  const isDrawing = useRef(false);
+
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { points: [pos.x, pos.y] }]);
+    console.log(pos)
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDrawing.current) return;
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    setLines((prevLines) => {
+      const lastLine = prevLines[prevLines.length - 1];
+      const newPoints = lastLine.points.concat([point.x, point.y]);
+      const updatedLines = [...prevLines.slice(0, -1), { points: newPoints }];
+      return updatedLines;
+    });
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
   return (
-    <>
-      <Stage></Stage>
-      <div>Whiteboard</div>
-    </>
-  )
-}
+    <main className="main">
+      <Stage
+        width={window.innerWidth}
+        height={400}
+        className="board"
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+      >
+        <Layer>
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="black"
+              strokeWidth={2}
+              lineCap="round"
+            />
+          ))}
+        </Layer>
+      </Stage>
+    </main>
+  );
+};
