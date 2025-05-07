46e8d785d079d12f8e5bbd149222cea96ce872d3
Jonathan <jona.rierasanchez@gmail.com>
2025-05-08 01:18:18 +0200

    removed remote logic

--- Frontend/src/pages/dashboard/StartStream.jsx
+++ Frontend/src/pages/dashboard/StartStream.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { socketService } from "../../services/socketService";
-export const StartStream = () => {
-  const { courseId } = useParams()
-  //My webcam
-  const localVideoRef = useRef(null)
-  //Other people webcam
-  const remoteVideoRef = useRef(null)
 
-  const remoteStreamRef = useRef(new MediaStream())
-  const peerConnectionRef = useRef(null)
+export const StartStream = () => {
+  const { courseId } = useParams();
+  //My webcam
+  const localVideoRef = useRef(null);
+  const peerConnectionRef = useRef(null);
 
   const servers = {
     iceServers: [
       {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+        urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
       },
     ],
     iceCandidatePoolSize: 10,
@@ -27,74 +24,55 @@ export const StartStream = () => {
     const pc = new RTCPeerConnection(servers);
     peerConnectionRef.current = pc;
 
-    if (remoteVideoRef.current) {
-      remoteVideoRef.current.srcObject = remoteStreamRef.current;
-    }
-    //rebre stream remot
-    pc.ontrack = (e) => {
-      e.streams[0].getTracks().forEach((track) => {
-        remoteStreamRef.current.addTrack(track);
-      })
-    }
-
-    pc.onicecandidate = (e) =>{
+    //enviar ice candidate
+    pc.onicecandidate = (e) => {
       if (e.candidate) {
         socketService.sendIceCandidate(courseId, e.candidate);
       }
-    }
-    return() => {
-      pc.close();
-      remoteStreamRef.current.getTracks().forEach((track) => {
-        track.stop();
-
-      remoteStreamRef.current = new MediaStream();
-  
-
-      })
     };
-  }, [courseId])
+
+    return () => {
+      pc.close();
+    };
+  }, [courseId]);
 
   const handleWebcamPermission = async () => {
-    try{
-    const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    localVideoRef.current.srcObject = stream;
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      localVideoRef.current.srcObject = stream;
 
-    stream.getTracks().forEach((track) => {
-      peerConnectionRef.current.addTrack(track, stream);
-    })
+      //añadir stream local a la conexio
+      stream.getTracks().forEach((track) => {
+        peerConnectionRef.current.addTrack(track, stream);
+      });
 
-    //crear offer despres d'obtenir els permisos
-    createOffer()
-  } catch (err) {
-    console.error("Error accessing webcam:", err);
-  }
-  }
+      //crear offer despres d'obtenir els permisos
+      createOffer();
+    } catch (err) {
+      console.error("Error accessing webcam:", err);
+    }
+  };
 
   const createOffer = async () => {
     try {
       const pc = peerConnectionRef.current;
       const offer = await pc.createOffer();
-      //aket objecte te un valor de stp
       await pc.setLocalDescription(offer);
-      socketService.sendOffer(courseId, offer); 
-    } catch (error) {
-      console.error("Error creating offer", err)
-      
+
+      //enviar offer
+      socketService.sendOffer(courseId, offer);
+    } catch (err) {
+      console.error("Error creating offer", err);
     }
-  }
+  };
+
   return (
     <>
-    <div>
-      <button id="webcam-btn" onClick={handleWebcamPermission}>Start Stream</button>
-      <h2>Streaming in course - Course: {courseId}</h2>
-      <video ref={localVideoRef} autoPlay playsInline muted style={{with: "300px"}}></video>
-    </div>
-
-    <div>
-      <h3>Remote Stream</h3>
-      <video ref={remoteVideoRef} autoPlay playsInline style={{with: "300px"}}></video>
-    </div>
+      <div>
+        <button id="webcam-btn" onClick={handleWebcamPermission}>Start Stream</button>
+        <h2>Streaming in course - Course: {courseId}</h2>
+        <video ref={localVideoRef} autoPlay playsInline muted style={{ width: "300px" }}></video>
+      </div>
     </>
-    
-  )
-}
+  );
+};
