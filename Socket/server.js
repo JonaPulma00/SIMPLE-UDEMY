fc7bc10b333d0fbff107eb596d8456ab1374befc
Jonathan <jona.rierasanchez@gmail.com>
2025-05-10 23:31:52 +0200

    Imported messages handler

--- Socket/server.js
+++ Socket/server.js
@@ -1,6 +1,7 @@
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 import { registerGeneralHandlers } from "./handlers/generalHandlers.js";
 import { registerStreamHandlers } from "./handlers/streamHandlers.js";
+import { registerMessagesHandlers } from "./handlers/messagesHandler.js";
 import dotenv from "dotenv";
 import { createSocketServer } from "./config/socket.js";
 
@@ -15,6 +16,7 @@ const io = createSocketServer({ port: PORT, origin: FRONT_URL });
 const onConnection = (socket) => {
   registerGeneralHandlers(io, socket);
   registerStreamHandlers(io, socket);
+  registerMessagesHandlers(io, socket);
 };
 if (!io) {
   console.error("Could not start socket server");
