ed5ab82bbc59885434c4db4fb580a823623f0775
Jonathan <jona.rierasanchez@gmail.com>
2025-05-10 23:31:14 +0200

    added message handler listener

---
+++ Socket/handlers/messagesHandler.js
@@ -0,0 +1,18 @@
+import { errorHandler } from "../middlewares/errorHandler";
+
+export const registerMessagesHandlers = (io, socket) => {
+  socket.on(
+    "send-message",
+    errorHandler((data) => {
+      const { groupId, message, id, username } = data;
+
+      if (!groupId || !message || !id || !username) return;
+      console.log(`Message received in group ${data.groupId}: ${data.message}`);
+      socket.to(groupId).emit("receive-message", {
+        id,
+        text: message,
+        username,
+      });
+    })
+  );
+};
