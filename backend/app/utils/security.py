1387af25f8eaa6790c52026441d4a16900c30b4e
Jonathan <jona.rierasanchez@gmail.com>
2025-05-15 20:15:05 +0200

    migrated redis to docker

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from app.core.config import settings
 from fastapi import HTTPException, status
 import redis 
-redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_client = redis.Redis(host="localhost", port=6380, decode_responses=True)
 
 
 def get_password_hash(password: str):
