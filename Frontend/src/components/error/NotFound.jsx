d8d658e1ceabaf4d7d9c5193a071aec087824ea7
Jonathan <jona.rierasanchez@gmail.com>
2025-05-17 19:02:43 +0200

    minor change

--- Frontend/src/components/error/NotFound.jsx
+++ Frontend/src/components/error/NotFound.jsx
@@ -4,7 +4,7 @@ export const NotFound = () => {
   return (
     <>
       <Navbar />
-      <h1>404 - Page Not Found</h1>
+      <h1>Error 404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </>
   )
