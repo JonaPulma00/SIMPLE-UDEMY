a0409067e28ddf0a548550cc0503b00e26308c39
Jonathan <jona.rierasanchez@gmail.com>
2025-05-18 18:24:56 +0200

    adding s3 upload pfp logic

--- backend/app/utils/profile_handler.py
+++ backend/app/utils/profile_handler.py
@@ -1,3 +1,18 @@
 import boto3
 from fastapi import HTTPException, status, UploadFile
 from app.core.config import settings
+
+class ProfileHandler:
+  def __init__(self):
+    self.s3_client = boto3.client(
+      's3',
+      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+      aws_session_token=settings.AWS_SESSION_TOKEN, 
+      region_name=settings.AWS_REGION
+    )
+    self.bucket_name = settings.AWS_BUCKET_NAME
+    
+  def get_profile_picture_path(self, user_id: str) -> str:
+    return f"users/{user_id}/profile_picture.jpg"
+    
