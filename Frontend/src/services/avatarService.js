52987b35910c6c6e9e0d5ebdc0bdf42f0e794308
Jonathan <jona.rierasanchez@gmail.com>
2025-05-18 17:29:45 +0200

    gave default values

--- Frontend/src/services/avatarService.js
+++ Frontend/src/services/avatarService.js
@@ -1,5 +1,10 @@
 const BASE_URL = import.meta.env.VITE_AVATAR_URL;
 
-export const getAvatarUrl = ({ name, background, color, size }) => {
+export const getAvatarUrl = ({
+  name,
+  background = "0D8ABC",
+  color = "fff",
+  size = "128",
+}) => {
   return `${BASE_URL}?name=${name}&background=${background}&color=${color}&size=${size}`;
 };
