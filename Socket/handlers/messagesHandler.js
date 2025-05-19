dfd9e385d9804f3da3c1add34dd3dc25dd929e1e
Jonathan <jona.rierasanchez@gmail.com>
2025-05-19 22:50:35 +0200

    standarizing rooms on messagehandler

--- Socket/handlers/messagesHandler.js
+++ Socket/handlers/messagesHandler.js
@@ -4,17 +4,15 @@ export const registerMessagesHandlers = (io, socket) => {
   socket.on(
     "send-message",
     errorHandler((data) => {
-      const { groupId, message, id, username } = data;
+      const { roomId, message, id, username } = data;
 
-      if (!groupId || !message || !id || !username) return;
-      console.log(`Message received in group ${data.groupId}: ${data.message}`);
-      socket.to(groupId).emit("receive-message", {
+      if (!roomId || !message || !id || !username) return;
+      console.log(`Message received in room ${roomId}: ${message}`);
+      socket.to(roomId.toString()).emit("receive-message", {
         id,
         text: message,
         username,
       });
     })
   );
-
-  
 };
