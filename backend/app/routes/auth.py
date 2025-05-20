b1d7b1a45ba7086769315d2694a985dbffcf9a6c
Jonathan <jona.rierasanchez@gmail.com>
2025-05-20 23:56:34 +0200

    removed nonsense oauth2

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request, Response
+from fastapi import APIRouter, Depends, Request, Response, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.controllers.auth_controller import register_user, authenticate_user, refresh_access_token, logout_user, authenticate_google_user
 from app.db.database import get_db
@@ -7,7 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 from app.middlewares.authenticate_token import verify_token
 
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
 
 @router.post("/register")
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
@@ -23,7 +23,14 @@ async def refresh(request: Request):
     return await refresh_access_token(request)
 
 @router.post("/logout")
-async def logout(request: Request, token: str = Depends(oauth2_scheme)):
+async def logout(request: Request, token_payload: dict = Depends(verify_token)):
+
+    auth_header = request.headers.get("Authorization")
+    token = auth_header.split(" ")[1] if auth_header and auth_header.startswith("Bearer ") else None
+    
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        
     return await logout_user(request, token)
 
 
